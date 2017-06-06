42a701fc495563ab756423b974eecd464035d17b
Floshy <leo.berteloot@gmail.com>
2017-06-06 19:41:04 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,11 @@
+files {
+    'index.html',
+    'keks.css',
+    'main.css',
+    'script.js',
+    'bankgothic.ttf'
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
